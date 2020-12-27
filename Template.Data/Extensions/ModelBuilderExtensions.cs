2e9956d3b6fdbfc9443396111fe06ce53da4c433
gdribeiro <gustavo.dias.ribeiro@gmail.com>
2020-12-27 17:20:45 -0300

    Configuração Entity Framework Core

---
+++ Template.Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Domain.Entities;
+
+namespace Template.Data.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder SeedData( this ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasData
+                (
+                    new User() { Id = Guid.Parse("b0c8fa0e-114a-4fd1-b8cd-f5d4eb3b7186"), Name = "User Default", Email = "userdefault@template.com" }
+                );
+            return builder;
+        }
+    }
+}
---
+++ Template.Data/Migrations/20201227201708_Inserting Default User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Template.Data.Migrations
+{
+    public partial class InsertingDefaultUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[] { new Guid("b0c8fa0e-114a-4fd1-b8cd-f5d4eb3b7186"), "userdefault@template.com", "User Default" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("b0c8fa0e-114a-4fd1-b8cd-f5d4eb3b7186"));
+        }
+    }
+}
