ce56f3e664a1cdb16315ff3c6ae5dd48b2b92367
gdribeiro <gustavo.dias.ribeiro@gmail.com>
2020-12-30 12:36:09 -0300

    Adicionando o AutoMapper no Projeto

---
+++ Template.Aplication/AutoMapper/AutoMapperSetup.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Aplication.ViewModels;
+using Template.Domain.Entities;
+
+namespace Template.Aplication.AutoMapper
+{
+    public class AutoMapperSetup : Profile
+    {
+        public AutoMapperSetup()
+        {
+            #region ViewModelToDomain
+            CreateMap<UserViewModel, User>();
+            #endregion
+            #region DomainToViewModel
+            CreateMap<User, UserViewModel>();
+            #endregion
+        }
+    }
+}
--- Template.Aplication/Services/UserService.cs
+++ Template.Aplication/Services/UserService.cs
@@ -6,39 +6,48 @@ using Template.Aplication.ViewModels;
 using Template.Domain.Entities;
 using Template.Domain.Interfaces;
 using System.Linq;
+using AutoMapper;
 
 namespace Template.Aplication.Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository userRepository;
-        public UserService(IUserRepository userRepository)
+        private readonly IMapper mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
         {
             this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
         public List<UserViewModel> Get()
         {
             List<UserViewModel> _usersViewModel = new List<UserViewModel>();
             IEnumerable<User> _users = userRepository.GetAll();
-            _users.ToList().ForEach(x => _usersViewModel.Add(new UserViewModel() 
-            { 
-                Id = x.Id,
-                Name = x.Name,
-                Email = x.Email
-            }));
+
+            _usersViewModel = mapper.Map<List<UserViewModel>>(_users);
+
+            //_users.ToList().ForEach(x => _usersViewModel.Add(new UserViewModel() 
+            //{ 
+            //    Id = x.Id,
+            //    Name = x.Name,
+            //    Email = x.Email
+            //}));
 
             return _usersViewModel;
         }
 
         public bool Post(UserViewModel userViewModel)
         {
-            User _user = new User
-            {
-                Id = new Guid(),
-                Name = userViewModel.Name,
-                Email = userViewModel.Email
-            };
+            //User _user = new User
+            //{
+            //    Id = new Guid(),
+            //    Name = userViewModel.Name,
+            //    Email = userViewModel.Email
+            //};
+
+            User _user = mapper.Map<User>(userViewModel);
 
             this.userRepository.Create(_user);
 
